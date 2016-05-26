ba75ea9a6e7c70155053fb0c381a5ec6e35e5568
cdwagner2727 <cdwagner2727@gmail.com>
2016-05-26 00:42:11 -0400

    Testing
    Testing

---
+++ Finder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSearchTwo
+{
+    class Finder
+    {
+        DirectoryInfo _dir;
+        long _count = 0;
+
+        public string Find(DirectoryInfo startDir, string toFind)
+        {
+            _count++;
+            _dir = startDir;
+            try
+            {
+                foreach (var file in _dir.GetFiles())
+                {
+                    if (file.Name == toFind)
+                    {
+                        return file.FullName;
+                    }
+                }
+
+                foreach (var dir in _dir.GetDirectories())
+                {
+                    _dir = dir;
+                    if (Find(_dir, toFind) != "not found")
+                    {
+                        return Find(_dir, toFind);
+                    }
+                }
+            }
+            catch { }
+            return "not found";
+        }
+
+        internal long getCount()
+        {
+            return _count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSearchTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finder finder = new Finder();
+            DirectoryInfo dir = new DirectoryInfo(@"C:\");
+            Console.WriteLine(finder.Find(dir, "Butt.txt"));
+            Console.WriteLine(finder.getCount());
+            Console.ReadLine();
+        }
+    }
+}
